ab343b9a618b0bbc7e53485ab259cef8f748320d
pouya <pouya.hadi1382@gmail.com>
2025-12-06 17:38:58 +0330

    ProductSort

--- lib/data/product.dart
+++ lib/data/product.dart
@@ -1,3 +1,10 @@
+class ProductSort {
+  static const int latest = 0;
+  static const int popular = 1;
+  static const int priceHighToLow = 2;
+  static const int priceLowToHigh = 3;
+}
+
 class ProductEntity {
   final int id;
   final String title;
